244d81763f33ebd32e5a14055862a36d93c4f414
ricardo <munch_141@hotmail.com>
2017-03-25 16:45:44 -0400

    detecta los segmentos de un pentagrama con algún símbolo

--- omr.py
+++ omr.py
@@ -23,49 +23,15 @@ def detect_staves(img_name):
 
     staves = sf.get_average()
     for i, staff in enumerate(staves):
-        print "Staff %d has %d staves:" % (i+1, len(staff))
+        #print "Staff %d has %d staves:" % (i+1, len(staff))
         for j, line in enumerate(staff):
             if j == 0:
                 ini = max(line.average_y-2*(d+n), 0)
             if j == len(staff)-1:
                 fin = min(line.average_y+2*(d+n), alto-1)
-            print "    %d. line at y-position:" % (j+1), line.average_y
+            #print "    %d. line at y-position:" % (j+1), line.average_y
         img2 = SubImage(image, Point(0, ini), Point(ancho-1, fin))
         staff_segments.append(img2)
-    return staff_segments, d, n
-
-
-def segment_staves(img_name):
-    """
-    Detecta y remueve las lineas de los pentagramas. Devuelve un arreglo con
-    los segmentos de la imagen donde estan los pentagramas.
-    """
-    init_gamera()
-
-    image = load_image(img_name)
-    image = image.to_onebit()
-    ancho = image.ncols
-
-    ms = musicstaves_rl_simple.MusicStaves_rl_simple(image)
-    ms.remove_staves(crossing_symbols='all', num_lines=5)
-    no_staves_img = ms.image
-
-    d = ms.staffspace_height
-    n = ms.staffline_height
-    staff_segments = []
-
-    staves = ms.get_staffpos()
-    for i, staff in enumerate(staves):
-        print "Staff %d has %d staves:" % (staff.staffno, len(staff.yposlist))
-        for j, y in enumerate(staff.yposlist):
-            if j == 0:
-                ini = y
-            if j == len(staff.yposlist)-1:
-                fin = y
-            print "    %d. line at y-position:" % (j+1), y
-        img2 = SubImage(no_staves_img, Point(0, ini), Point(ancho-1, fin))
-        staff_segments.append(img2)
-
     return staff_segments, d, n
 
 
@@ -96,10 +62,25 @@ if __name__ == '__main__':
     for i, staff in enumerate(staves):
         staff_segments.append(staff)
         staff.save_PNG("imagenes/staff_segments/staff{0}.png".format(i+1))
-    print "space height: ", space
-    print "line height: ", line
 
     # LEVEL 0 SEGMENTATION
+    level0 = []
+    #for s in staff_segments:
+    s = staff_segments[0]
+    xp = s.projection_cols()
+    ini = 0
+    in_segment = False
+    for i in range(len(xp)):
+        if not in_segment and xp[i] > 5*line:
+            ini = i
+            in_segment = True
+        elif in_segment and xp[i] <= 5*line:
+            level0.append((ini, i))
+            img2 = SubImage(s, Point(ini, s.offset_y), Point(i, s.offset_y+s.nrows-1))
+            img2.save_PNG("imagenes/staff_segments/level0/{0}.png".format(len(level0)))
+            in_segment = False
+
+
 
 
 
